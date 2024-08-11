1537e165916cb447c682d7ba4e34e445e1dfadde
Seitoni <sietonsen@gmail.com>
2024-08-11 22:17:51 +0300

    Add files via upload

---
+++ Phrase_generator.js
@@ -0,0 +1,24 @@
+function makephrase() {
+
+  var words1 = ["24/7", "multi-tier", "30,000 foot", "B-to-B", "win-win"];
+  var words2 = ["empovered", "value-added", "oriented", "focused", "aligned"];
+  var words3 = ["process", "solution", "tipping-point", "strategy", "vision"];
+
+  var rand1 = Math.floor (Math.random() * words1.length);
+  var rand2 = Math.floor (Math.random() * words2.length);
+  var rand3 = Math.floor (Math.random() * words3.length);
+
+
+  var phrase = words1[rand1] + "  " + words2[rand2] + "  " + words3[rand3];
+
+
+  alert ("It's your beautiful generate phrases:" + "  " + phrase);
+
+ /*[--Variation2--] 
+ var phrase = [words1[rand1], words2[rand2], words3[rand3]];*/
+
+/*[--Variation2--] 
+ alert ("It's your beautiful generate phrases:" + "  " + phrase[0] + "  " + phrase[1] + "  " + phrase[2]);*/
+}
+
+makephrase ();
